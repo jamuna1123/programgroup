e2d4274d0e70559bdf81744f487a779b544ff912
jamuna1123 <104769314+jamuna1123@users.noreply.github.com>
2022-05-07 18:25:14 +0545

    Add files via upload

---
+++ index.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
+
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+
+     <img src="images/logo1.png" alt=" " height="80" width="100" >
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <nav class="navbar navbar-inverse navbar-fixed-top">
+
+      
+       
+      
+      </form>
+    </div>
+  </div>
+</nav>
+<div class="mb-3 fashion" style="margin-left:30px">
+ <a class="navbar-brand" href="#">Fashion</a>
+</div> 
+
+    <div class="main bg-warning">
+        <div class="fashion container">
+            <div class="row">
+                <div class="col-md-6 text">
+                    <h1 class="fw-bold">Fashion <br>Spring 2019</h1>
+                    <p><blockquote>"Happy people make happy clothes",</blockquote>Wes Gordon said backstage.
+                    He was talking about the most important lesson he learned from Carolina Herrera in the year and a half that he worked by her side before his runway debut today.
+                </p>
+                <a href="#" class="btn">view more</a>
+                </div>
+                <div class="col-md-6 img">
+                    <img src="images/1.jpg" alt="img">
+                </div>
+            
+        </div>
+    </div>
+</div>
+<br>
+    <div class="fashion container">
+<img src="images/2.jpg" alt="img">
+<div class="col-md-6 text">
+                    <h1 class="fw-bold">Florals Create The Perfect Illusion</h1>
+                    
+                </div>
+</div> 
+<div class="main bg-light">
+        <div class="fashion container bg-warning">
+            <div class="row">
+                <div class="col-md-7 text" >
+                    <p><blockquote>"Happy people make happy clothes",</blockquote>Wes Gordon said backstage.
+                    He was talking about the most important lesson he learned from Carolina Herrera in the year and a half that he worked by her side before his runway debut today.<br>
+                    <br> <br> Photo by wwd.com
+                </p>
+                </div>
+</div>
+</div>
+<br>
+                <div class="col-md-6 img" style="display:flex">
+                    <img src="images/3.jpg" alt="img" width="700">
+                 <br><br>&nbsp;&nbsp;&nbsp;&nbsp;
+                    <img src="images/4.jpg"  >
+                </div>
+            
+        </div>
+        <div class="p-3 mb-2 bg-danger text-white text-center " style="height:700px">
+            <h1>Carolina Herrera Fashion Show</h1><br>
+            <img src="images/5.jpg" >
+            <br><br>
+            <p>Well-traveled, fashionable couples with a keen eye for detail. They want to create the ultimate wedding experience, infused with thoughtfulness. Usually, it's a multi-day marriage celebration at a meaningful, remote destination. They’re sourcing an elite team of wedding planners, designers, and florists to help bring their dream wedding to life, visually and experientially.</p>
+        </div>
+<br>
+    <div class="text-black text-center">
+    <h1>Florals Create The Perfect Illusion</h1>
+    <br>
+    <div class="col-md-6 img img-center" style="display:flex">
+
+  <img src="images/7.jpg" width="700" height="600"><br>
+  &nbsp;
+   <img src="images/6.jpg" width="500" height="600">
+   
+</div>
+<br><br>
+<div class="text-black">
+<p>  Carolina Herrera: 35 Years of Fashion is both a portrait of a universally acclaimed American designer’s career and a look forward to her future. The selection of images includes editorial pictures by renowned fashion photographers, stylists, and editors, as well as two portfolios of new photography</p>
+</div>
+</div>
+<br>
+<div class="p-3 mb-2 bg-info text-white text-center" style="height:600px">
+<h1>Carolina Herrera:35 Years Of Fashion</h1>
+<a href="#" class="btn ">learn more</a>
+<img src="images/8.jpg" width="700" img="center">
+
+</div>
+</div>
+
+<div class="footer-basic">
+        <footer>
+            <div class="social"><a href="#"><i class="icon ion-social-instagram"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-facebook"></i></a></div>
+            
+            <p class="copyright">© 2018 Carolina Herrera</p>
+            <div class="bg-dark py-4">
+      <div class="container text-center">
+       <a href="#">websites template</a> <p class="text-muted mb-0 py-2">created with.</p>
+       <a href="#">websites buiding software</a>
+      </div>
+    </div>
+        </footer>
+</div>
+        
+    
+</body>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</html>
